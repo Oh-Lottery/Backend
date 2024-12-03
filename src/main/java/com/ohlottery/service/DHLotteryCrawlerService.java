2017c2cacfd050545ff144068fcb524550ec2a97
SalkCoding <josangbeom1104@gmail.com>
2024-12-04 00:34:40 +0900

    Init commit

--- src/main/java/com/ohlottery/service/DHLotteryCrawlerService.java
+++ src/main/java/com/ohlottery/service/DHLotteryCrawlerService.java
@@ -12,10 +12,18 @@ public class DHLotteryCrawlerService {
     private final Lottery645Repository lottery645Repository;
     private final Lottery720Repository lottery720Repository;
 
+    public void fetchLottery645Data(int round){
+
+    }
+
+    public void fetchLottery720Data(int round){
+
+    }
+
     /* TODO
     동행 복권 사이트 엔드포인트 긁어오기
-    6/45는 매주 토요일 20시 35분
-    720은 매주 목요일 19시 5분
+    6/45는 매주 토요일 20시 35분 -> 5분 후
+    720은 매주 목요일 19시 5분 -> 5분 후
 
     기본 데이터는 엑셀 파일에서 긁어와서 데이터베이스에 마이그레이션
     */
