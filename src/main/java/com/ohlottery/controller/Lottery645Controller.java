3c30f14d7ffc64901e8ff18f458ae45ae5ae5d30
SalkCoding <josangbeom1104@gmail.com>
2024-12-05 23:08:57 +0900

    Swagger is applied and APIs are documented

--- src/main/java/com/ohlottery/controller/Lottery645Controller.java
+++ src/main/java/com/ohlottery/controller/Lottery645Controller.java
@@ -3,6 +3,13 @@ package com.ohlottery.controller;
 import com.ohlottery.dto.Lottery645Dto;
 import com.ohlottery.entity.Lottery645Entity;
 import com.ohlottery.service.LotteryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,14 +20,27 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("lottery645")
+@RequestMapping("lottery645/round")
 @RequiredArgsConstructor
+
+@Tag(name = "Lottery 6/45 Controller", description = "6/45 당첨 정보 API")
 public class Lottery645Controller {
 
     private final LotteryService lotteryService;
 
     @GetMapping("/{round}")
-    public ResponseEntity<?> getLotteryNumber(@PathVariable("round") String round) {
+
+    @Operation(summary = "6/45로또 데이터 조회", description = "해당 회차 6/45 당첨 정보를 데이터베이스에서 조회")
+    @ApiResponses(value ={
+            @ApiResponse(responseCode = "200", description = "데이터 조회 성공",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Lottery645Dto.class))),
+            @ApiResponse(responseCode = "400", description = "잘못된 인자 값", content = @Content()),
+            @ApiResponse(responseCode = "404", description = "해당 회차 당첨 정보가 없음", content = @Content())
+    })
+    public ResponseEntity<?> getLotteryNumber(
+            @Parameter(description = "회차", example = "1")
+            @PathVariable("round") String round
+    ) {
         long requestRound;
         try {
             requestRound = Long.parseLong(round);
@@ -31,7 +51,7 @@ public class Lottery645Controller {
         Optional<Lottery645Entity> roundResult
                 = lotteryService.getLottery645Result(requestRound);
 
-        if (roundResult.isEmpty()) return ResponseEntity.badRequest().build();
+        if (roundResult.isEmpty()) return ResponseEntity.notFound().build();
 
         Lottery645Entity resultEntity = roundResult.get();
         Lottery645Dto resultDto = new Lottery645Dto(resultEntity);
