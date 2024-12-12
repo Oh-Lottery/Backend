280ab0a64eb3369dd8e90620635512de0183ae44
SalkCoding <josangbeom1104@gmail.com>
2024-12-13 02:33:04 +0900

    크롤링 시 네트워크 사용량을 줄이기 위해 end-point를 활용하여 크롤링 하도록 변경, 모든 데이터를 전부 저장할 수 있는 end-point 작성 /crawler/lottery/fetch -> 부하량 좀 있으니 막쓰면 안됨

--- src/main/java/com/ohlottery/controller/LotteryCrawlerController.java
+++ src/main/java/com/ohlottery/controller/LotteryCrawlerController.java
@@ -1,6 +1,9 @@
 package com.ohlottery.controller;
 
 import com.ohlottery.service.DHLotteryCrawlerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,22 +13,32 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/crawler")
 @RequiredArgsConstructor
+@Tag(name = "Lottery Crawler Controller", description = "당첨 정보 수동 크롤링")
 public class LotteryCrawlerController {
 
     private final DHLotteryCrawlerService lotteryCrawlerService;
 
+    @Operation(summary = "6/45로또 데이터 업데이트", description = "해당 회차 6/45 당첨 정보를 데이터베이스에 저장")
     @GetMapping("/lottery645/{round}")
-    public ResponseEntity<Void> fetchLottery645(@PathVariable long round) {
+    public ResponseEntity<Void> fetchLottery645(
+            @Parameter(description = "회차", example = "1")
+            @PathVariable long round
+    ) {
         lotteryCrawlerService.fetchLottery645Data(round);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "연금복권 데이터 업데이트", description = "해당 회차 6/45 당첨 정보를 데이터베이스에 저장")
     @GetMapping("/lottery720/{round}")
-    public ResponseEntity<Void> fetchLottery720(@PathVariable long round) {
+    public ResponseEntity<Void> fetchLottery720(
+            @Parameter(description = "회차", example = "1")
+            @PathVariable long round
+    ) {
         lotteryCrawlerService.fetchLottery720Data(round);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "모든 로또 데이터 업데이트", description = "1~n 회차까지 전부 데이터베이스에 저장")
     @GetMapping("/lottery/fetch")
     public ResponseEntity<Void> fetchAllData() {
         lotteryCrawlerService.fetchAllLotteryData();
